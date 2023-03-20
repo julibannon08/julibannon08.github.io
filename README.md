86d4a47db5476b0c636b2780dfebe74465d727b3
julibannon08 <113955076+julibannon08@users.noreply.github.com>
2023-03-19 22:56:07 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# julibannon08.github.io
---
+++ index.js
@@ -0,0 +1,8 @@
+//Botones de seccion "Contacto"
+document.getElementById('boton_telefono').addEventListener('click' , function() {
+    document.getElementById('numero_tel').style.display = 'inline-block'
+})
+
+document.getElementById('boton_email').addEventListener('click' , function() {
+    document.getElementById('email').style.display = 'inline-block'
+})
